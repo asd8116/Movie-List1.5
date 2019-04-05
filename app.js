b641e4d1e01281394f8673ae1486074400870c6f
Zen YI <asd8116@gmail.com>
2019-04-05 22:29:27 +0800

    rename

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
 // variables
-const express = require('express')
-const exphbs = require('express-handlebars')
-const restList = require('./restaurant.json')
-const app = express()
-const port = 3000
+const express = require("express");
+const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
+const app = express();
+const port = 3000;
 
 // template engine
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}))
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 
-app.set('view engine', 'handlebars')
+app.set("view engine", "handlebars");
 
 // static files
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // routes
-app.get('/', (req, res) => {
-  res.render('index', {
-    restaurants: restList.results
-  })
-})
+app.get("/", (req, res) => {
+  res.render("index", {
+    restaurants: restaurantList.results
+  });
+});
 
-app.get('/restaurants/:rest_id', (req, res) => {
-  const resChoose = restList.results.filter(rest => rest.id == req.params.rest_id)
+app.get("/restaurants/:rest_id", (req, res) => {
+  const resChoose = restaurantList.results.filter(
+    rest => rest.id == req.params.rest_id
+  );
 
-  res.render('show', {
+  res.render("show", {
     restaurant: resChoose[0]
-  })
-})
+  });
+});
 
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const resInput = restList.results.filter(rest => {
-    return rest.name.toLowerCase().includes(keyword.toLowerCase())
-  })
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const resInput = restaurantList.results.filter(rest => {
+    return rest.name.toLowerCase().includes(keyword.toLowerCase());
+  });
 
-  res.render('index', {
+  res.render("index", {
     restaurants: resInput,
     keyword: keyword
-  })
-})
+  });
+});
 // start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
-})
+  console.log(`Express is listening on localhost:${port}`);
+});
