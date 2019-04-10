1d81b3f503b18deb7a2809e59769891fd78d3c0d
wanaka <asd8116@gmail.com>
2019-04-10 09:58:35 +0800

    Add Mongoose connection and create model

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 // variables
-const express = require("express");
-const exphbs = require("express-handlebars");
-const restaurantList = require("./restaurant.json");
-const app = express();
-const port = 3000;
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const Restaurants = require('./models/restaurants')
+const app = express()
+const port = 3000
+
+// setting mongoose
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurants', {
+  useNewUrlParser: true
+})
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // template engine
-app.engine(
-  "handlebars",
+app.engine("handlebars",
   exphbs({
     defaultLayout: "main"
   })
 );
 
-app.set("view engine", "handlebars");
+app.set("view engine", "handlebars")
 
 // static files
-app.use(express.static("public"));
+app.use(express.static("public"))
 
 // routes
 app.get("/", (req, res) => {
@@ -33,20 +48,20 @@ app.get("/restaurants/:rest_id", (req, res) => {
   res.render("show", {
     restaurant: resChoose[0]
   });
-});
+})
 
 app.get("/search", (req, res) => {
   const keyword = req.query.keyword;
   const resInput = restaurantList.results.filter(rest => {
     return rest.name.toLowerCase().includes(keyword.toLowerCase());
-  });
+  })
 
   res.render("index", {
     restaurants: resInput,
     keyword: keyword
   });
-});
+})
 // start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`);
-});
+  console.log(`Express is listening on http://localhost:${port}`);
+})
