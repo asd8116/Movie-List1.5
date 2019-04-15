a000ba9adb3b5c450266f838b1b60f93d7ad33da
wanaka <asd8116@gmail.com>
2019-04-15 15:21:49 +0800

    Revert "Add restaurants and home routers"
    This reverts commit 8e5e396cfb92232ffa105620ff1cec93db85cfb0.

--- app.js
+++ app.js
@@ -40,8 +40,104 @@ app.use(bodyParser.urlencoded({
 app.use(methodOverride('_method'))
 
 // routes
-app.use('/', require('./routes/home'))
-app.use('/restaurants', require('./routes/restaurants'))
+app.get("/", (req, res) => {
+  Restaurants.find((err, restaurants) => {
+    if (err) return console.error(err)
+    return res.render('index', {
+      restaurants: restaurants
+    })
+  })
+})
+
+// 新增一筆 餐廳 頁面
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+// 新增一筆 餐廳
+app.post('/restaurants', (req, res) => {
+  const restaurant = Restaurants({
+    name: req.body.name,
+    category: req.body.category,
+    image: req.body.image,
+    location: req.body.location,
+    phone: req.body.phone,
+    google_map: req.body.google_map,
+    rating: req.body.rating,
+    description: req.body.description,
+  })
+
+  restaurant.save(err => {
+    if (err) return console.error(err)
+    return res.redirect('/')
+  })
+})
+
+// 顯示一筆 餐廳 的詳細內容
+app.get("/restaurants/:id", (req, res) => {
+  Restaurants.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    return res.render('show', {
+      restaurant: restaurant
+    })
+  })
+})
+
+// 修改 餐廳 頁面
+app.get('/restaurants/:id/edit', (req, res) => {
+  Restaurants.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    return res.render('edit', {
+      restaurant: restaurant
+    })
+  })
+})
+
+// 修改 餐廳
+app.put('/restaurants/:id', (req, res) => {
+  Restaurants.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    restaurant.name = req.body.name
+    restaurant.category = req.body.category
+    restaurant.image = req.body.image
+    restaurant.location = req.body.location
+    restaurant.phone = req.body.phone
+    restaurant.google_map = req.body.google_map
+    restaurant.rating = req.body.rating
+    restaurant.description = req.body.description
+    restaurant.save(err => {
+      if (err) return console.error(err)
+      return res.redirect(`/restaurants/${req.params.id}`)
+    })
+  })
+})
+
+// 刪除 餐廳
+app.delete('/restaurants/:id/delete', (req, res) => {
+  Restaurants.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    restaurant.remove(err => {
+      if (err) return console.error(err)
+      return res.redirect('/')
+    })
+  })
+})
+
+// Search
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword
+  Restaurants.find({
+    name: {
+      $regex: keyword,
+      $options: 'i'
+    }
+  }, (err, restaurant) => {
+    if (err) return console.error(err)
+    return res.render('index', {
+      restaurants: restaurant
+    })
+  })
+})
 
 // start and listen
 app.listen(port, () => {
